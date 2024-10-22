b7f1a3e16785ff7deed2370f75f011b7bd7b2957
JulianS201 <segurajulian71@gmail.com>
2024-10-21 20:29:47 -0600

    Semana 2, Semana 3, Semana 4

---
+++ src/main/java/com/tienda/mycontroller.java
@@ -0,0 +1,11 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mycontroller {
+
+    @GetMapping("/pagina2")
+    public String getPagina2() {
+        return "pagina2";  // 'pagina2' debe coincidir con el nombre del archivo en templates (sin .html)
+    }
+}
---
+++ src/test/java/com/tienda/mycontroller.java
@@ -0,0 +1,14 @@
+package com.tienda;
+
+import com.tienda.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mycontroller {
+
+    @GetMapping("/pagina2")
+    public String getPagina2() {
+        return "pagina2";  // 'pagina2' debe coincidir con el nombre del archivo en templates (sin .html)
+    }
+}
